4003ec9d6d3ef9ce611271719cc1d06b0598affd
xiaomile <15622388695@163.com>
2024-01-14 16:35:15 +0800

    Update model_download.py

--- model_download.py
+++ model_download.py
@@ -1,7 +1,7 @@
 # 下载InternLM-chat-7b模型
 import torch
-from modelscope import snapshot_download, AutoModel, AutoTokenizer
-model_dir = snapshot_download('Shanghai_AI_Laboratory/internlm-chat-7b', cache_dir='/home/xlab-app-center/model', revision='v1.0.3')
+#from modelscope import snapshot_download, AutoModel, AutoTokenizer
+#model_dir = snapshot_download('Shanghai_AI_Laboratory/internlm-chat-7b', cache_dir='/home/xlab-app-center/model', revision='v1.0.3')
 
-#from openxlab.model import download 
-#download(model_repo='xiaomile/personal_assistant2',output='/home/xlab-app-center/model')
+from openxlab.model import download 
+download(model_repo='xiaomile/personal_assistant2',output='/home/xlab-app-center/model')
